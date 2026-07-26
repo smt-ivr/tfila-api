f8fe0de1a9e6f8565fdfd28f69f65b2a1bfecbe7
smt-ivr <s6754091@gmail.com>
2026-07-26 22:36:30 +0300

    Delete hash.js

--- hash.js
+++
@@ -1,7 +0,0 @@
-export async function hashPassword(password) {
-    const salt = 'SMTI_SECRET_SALT_2026'; 
-    const msgBuffer = new TextEncoder().encode(password + salt);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-}
