aea852e68254e90acd0caf684124056dc558e535
smt-ivr <s6754091@gmail.com>
2026-07-27 20:20:07 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -8,20 +8,7 @@ export function getCurrentIsraelTime() {
     return { date, time };
 }
 
-export function calculateLateMinutes(arrivedTime, targetTime) {
-    const [arrH, arrM] = arrivedTime.split(':').map(Number);
-    const [tarH, tarM] = targetTime.split(':').map(Number);
-    
-    const arrivedTotal = arrH * 60 + arrM;
-    const targetTotal = tarH * 60 + tarM;
-    
-    const lateMinutes = arrivedTotal - targetTotal;
-    
-    return Math.max(0, lateMinutes);
-}
-
-// פונקציה לבדיקה האם התאריך נופל על יום שבת
 export function isSaturday(dateString) {
     const d = new Date(dateString);
-    return d.getDay() === 6; // 0=ראשון, ..., 6=שבת
+    return d.getDay() === 6;
 }
